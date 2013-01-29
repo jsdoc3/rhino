819a02a9dd74f3a8f18f48560850e640566b1aac
Klaus Rennecke <a.lagopus@gmail.com>
2013-01-29 17:29:13 +0100

    implement global scope "global" as in nodejs

--- toolsrc/org/mozilla/javascript/tools/shell/Global.java
+++ toolsrc/org/mozilla/javascript/tools/shell/Global.java
@@ -122,6 +122,9 @@ public class Global extends ImporterTopLevel
         history = (NativeArray) cx.newArray(this, 0);
         defineProperty("history", history, ScriptableObject.DONTENUM);
 
+        // global reference to global, as in nodejs
+        defineProperty("global", this, ScriptableObject.DONTENUM);
+
         initialized = true;
     }
 
@@ -1176,6 +1179,11 @@ public class Global extends ImporterTopLevel
         String message = ToolErrorReporter.getMessage(msgId, msgArg);
         return Context.reportRuntimeError(message);
     }
+
+	@Override
+	public String getClassName() {
+        return "JavaImporter";
+	}
 }
 
 

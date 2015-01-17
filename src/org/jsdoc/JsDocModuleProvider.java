e2c9c82b03268ad9560fbc06edc08fa889815f56
Jeff Williams <jeffrey.l.williams@gmail.com>
2015-01-17 11:20:23 -0800

    allow modules to require() from their child node_modules directory
    Note that this type of require() must be at the top level of the module's source file. A require() nested within a function body does not currently work.

--- src/org/jsdoc/JsDocModuleProvider.java
+++ src/org/jsdoc/JsDocModuleProvider.java
@@ -134,19 +134,30 @@ public class JsDocModuleProvider extends UrlModuleSourceProvider {
 		throws SecurityException, IOException, ParseException, URISyntaxException {
 		URI submoduleUri = null;
 		String currentModule = getCurrentModuleUri().toString();
+		
+		// Find the child submodule, if any.
+		URI childSubmoduleUri = new URI(currentModule + SUBMODULE_DIRECTORY);
+		File childSubmoduleDir = new File(childSubmoduleUri);
 
-		// Find the nearest parent module, if any.
-		int submoduleParentIndex = currentModule.lastIndexOf(SUBMODULE_DIRECTORY + PATH_SEPARATOR);
-		if (submoduleParentIndex != -1) {
-			int parentIndex = currentModule.indexOf(PATH_SEPARATOR, submoduleParentIndex +
-				SUBMODULE_DIRECTORY.length() + 1);
-			if (parentIndex != -1) {
-				String parentModule = currentModule.substring(0, parentIndex);
-				String submoduleDir = parentModule + PATH_SEPARATOR + SUBMODULE_DIRECTORY;
+		if (childSubmoduleDir.isDirectory()) {
+			submoduleUri = getModuleUri(new URI(childSubmoduleUri.toString() + PATH_SEPARATOR +
+				base.relativize(uri).toString()), childSubmoduleUri, false);
+		}
+		
+		if (submoduleUri == null) {
+			// Find the nearest parent module, if any.
+			int submoduleParentIndex = currentModule.lastIndexOf(SUBMODULE_DIRECTORY + PATH_SEPARATOR);
+			if (submoduleParentIndex != -1) {
+				int parentIndex = currentModule.indexOf(PATH_SEPARATOR, submoduleParentIndex +
+					SUBMODULE_DIRECTORY.length() + 1);
+				if (parentIndex != -1) {
+					String parentModule = currentModule.substring(0, parentIndex);
+					String submoduleDir = parentModule + PATH_SEPARATOR + SUBMODULE_DIRECTORY;
 
-				URI submoduleSearchUri = new URI(submoduleDir + PATH_SEPARATOR +
-					base.relativize(uri).toString());
-				submoduleUri = getModuleUri(submoduleSearchUri, base, false);
+					URI submoduleSearchUri = new URI(submoduleDir + PATH_SEPARATOR +
+						base.relativize(uri).toString());
+					submoduleUri = getModuleUri(submoduleSearchUri, base, false);
+				}
 			}
 		}
 
